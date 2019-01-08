f8e373a0e08e1935e7bde798ca16f4a87a68853b
Benny <Benny@10.0.0.12>
2019-01-08 20:44:41 +0100

    + 20er Reihen

--- HalloWeltProjekt/src/basics/Schleife.java
+++ HalloWeltProjekt/src/basics/Schleife.java
@@ -6,7 +6,7 @@ public class Schleife {
 		// 1. variable dekleration & zuweisung
 		// 2. die bedinung
 		// 3. increment / decrement
-		for (int i = 0; i < 10; i=i+1) {
+		for (int i = 1; i < 11; i=i+1) {
 			System.out.println("Zahl: " + i);
 		} 
 	}
