135200a610766dd625d4d7f4123ce86636d07248
benedikt.beck <benedikt.beck@AMS-GYM-EDV1-14.gym.ams.local>
2019-01-21 16:16:53 +0100

    + WhileSchleife

---
+++ HalloWeltProjekt/src/basics/WhileSchleife.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class WhileSchleife {
+	public static void main(String[] args) {
+		// while...
+		int zahl1 = 120;
+		int zahl2 = zahl1;
+
+		while (zahl1 > 0) {
+			if ((zahl2 % zahl1) == 0)
+				System.out.println("Das ist ein Teiler: " + zahl1 + "!");
+			zahl1 = zahl1 - 1;
+
+		}
+
+	}
+
+}
